df99f64e662aaa3184b08d790d8aff3d5157007b
MajdSoubh <majdsoubh53@gmail.com>
2025-02-16 13:11:03 +0300

    [DEPS] [ASSETS] Added assets and install required library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,25 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
-  css: [
-    "~/node_modules/bootstrap/dist/css/bootstrap.min.css",
-    "~/assets/css/main.css",
-  ],
-  plugins: [
-    {
-      src: "~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
-      mode: "client",
+  devtools: { enabled: false },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
+        },
+      ],
+      script: [
+        {
+          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
+          tagPosition: "bodyClose",
+        },
+      ],
     },
-  ],
+  },
+  alias: {
+    "@images": "~/assets/images",
+  },
+
+  css: ["~/assets/css/main.css", "~/assets/css/reset.css"],
 });
